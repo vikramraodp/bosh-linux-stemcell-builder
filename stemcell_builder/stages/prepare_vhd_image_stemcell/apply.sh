945acc99f2efef6cbbfb8a553da57581a096caf3
Andre Duffeck <andre@duffeck.de>
2017-08-18 11:04:17 +0200

    Fix fixing size of the VHD to whole megabytes
    Some versions of qemu-img need the `subformat=force_size` option to make it work.
    
    Only use force_size in opensuse_leap containers

--- stemcell_builder/stages/prepare_vhd_image_stemcell/apply.sh
+++ stemcell_builder/stages/prepare_vhd_image_stemcell/apply.sh
@@ -8,7 +8,11 @@ source $base_dir/lib/prelude_apply.bash
 rm -f $work/root.vhd
 
 # The size of the VHD for Azure must be a whole number in megabytes.
-qemu-img convert -O vpc -o subformat=fixed $work/${stemcell_image_name} $work/root.vhd
+if grep NAME=\"openSUSE\ Leap\" /etc/os-release; then
+    qemu-img convert -O vpc -o subformat=fixed,force_size $work/${stemcell_image_name} $work/root.vhd
+else
+    qemu-img convert -O vpc -o subformat=fixed $work/${stemcell_image_name} $work/root.vhd
+fi
 
 pushd $work
 tar zcf stemcell/image root.vhd
