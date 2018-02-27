f6b7246d75dce32d7b8dd8dcb064e69fc870e83d
Tim Hardeck <thardeck@suse.de>
2018-02-27 14:41:56 +0100

    Adapt os-image build script to have a flexible user
    but use ubuntu by default so regular pipelines do not need to be
    adapted.

--- ci/os-images/tasks/build.sh
+++ ci/os-images/tasks/build.sh
@@ -3,6 +3,7 @@
 set -eu
 
 TASK_DIR=$PWD
+TARGET_USER="${TARGET_USER:-ubuntu}"
 
 cd bosh-linux-stemcell-builder
 
@@ -20,9 +21,9 @@ check_param OPERATING_SYSTEM_VERSION
 OS_IMAGE_NAME=$OPERATING_SYSTEM_NAME-$OPERATING_SYSTEM_VERSION
 OS_IMAGE=$TASK_DIR/os-image/$OS_IMAGE_NAME.tgz
 
-sudo chown -R ubuntu .
+sudo chown -R $TARGET_USER .
 sudo chmod u+s $(which sudo)
-sudo --preserve-env --set-home --user ubuntu -- /bin/bash --login -i <<SUDO
+sudo --preserve-env --set-home --user $TARGET_USER -- /bin/bash --login -i <<SUDO
     bundle install --local
     bundle exec rake stemcell:build_os_image[$OPERATING_SYSTEM_NAME,$OPERATING_SYSTEM_VERSION,$OS_IMAGE]
 SUDO
