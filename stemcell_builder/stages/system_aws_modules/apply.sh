488694fb22b91be28515a25f62d678e7b52be5be
André Duffeck <aduffeck@suse.com>
2018-06-08 10:43:48 +0200

    Add xen related modules to the initrd for the AWS stemcell

--- stemcell_builder/stages/system_aws_modules/apply.sh
+++ stemcell_builder/stages/system_aws_modules/apply.sh
@@ -6,3 +6,11 @@ base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
 echo "acpiphp" >> $chroot/etc/modules
+
+if [ "${stemcell_operating_system}" == "opensuse" ] || [ "${stemcell_operating_system}" == "sles" ]; then
+  cat > ${chroot}/etc/dracut.conf.d/xen.conf <<EOF
+add_drivers+="ata_piix ata_generic xen_vnif xen_vbd xen_platform_pci virtio_blk virtio_scsi virtio_net virtio_pci virtio_ring virtio"
+EOF
+  kernelver=$( ls -rt $chroot/lib/modules | tail -1 )
+  run_in_chroot $chroot "dracut --force --kver ${kernelver}"
+fi
