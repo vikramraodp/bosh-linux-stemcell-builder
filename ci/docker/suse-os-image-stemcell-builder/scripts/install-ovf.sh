6336e3fadede198cdb600ef3bac559af13752564
Vikram Rao <vikramdp.rao@gmail.com>
2022-03-05 20:33:15 +0000

    Update opensuse leap to 15.3 release

--- ci/docker/suse-os-image-stemcell-builder/scripts/install-ovf.sh
+++ ci/docker/suse-os-image-stemcell-builder/scripts/install-ovf.sh
@@ -6,5 +6,5 @@ cd /tmp
 echo "${OVF_TOOL_INSTALLER_SHA1} /tmp/ovftool_installer.bundle" | sha1sum -c -
 chmod a+x ./ovftool_installer.bundle
 ln -s /bin/cat /usr/local/bin/more
-echo -e "\nyes\n\n" | bash  ./ovftool_installer.bundle
+echo -e "\nyes\n\n" | bash  ./ovftool_installer.bundle --console --required --eulas-agreed
 rm -rf ./ovftool_installer.bundle /tmp/vmware-root/ /usr/local/bin/more
